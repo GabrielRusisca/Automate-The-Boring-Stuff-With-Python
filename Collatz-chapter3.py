b666d3658415dbcc10738ddf5fa72d35ce9698f1
Gabriel Rusisca Marques <grusiscam@gmail.com>
2022-06-22 15:25:11 -0300

    Adição do Código Collatz

--- Collatz-chapter3.py
+++ Collatz-chapter3.py
@@ -1,1 +1,20 @@
-print('Hello World!')
+def collatz(number):
+    if number % 2 ==0:
+        x = number // 2
+    else:
+        x = number * 3 + 1
+    print(x)
+    return x
+
+'''numero = collatz(int(input('Digite um número:\n')))
+while numero != 1:
+    numero = collatz(numero)''' # Até aqui é a parte 1
+
+numero = 2.5
+while numero != 1:
+    try:
+        while numero % 1 != 0:
+                numero = collatz(int(input('Digite um número:\n')))
+        numero = collatz(numero)
+    except ValueError:
+        print('Por favor tente de novo com um número inteiro!')  # Parte 2 (uso do try e do except) termina aqui

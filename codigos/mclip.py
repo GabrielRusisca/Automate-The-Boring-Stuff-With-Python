507806983d305b961469c34b7cfa0b873cc85b4b
Gabriel Rusisca Marques <grusiscam@gmail.com>
2022-07-30 01:02:15 -0300

    Primeiro Script

--- codigos/mclip.py
+++ codigos/mclip.py
@@ -1,6 +1,16 @@
 #! python3
 # mclip.py - A multi-clipboard program
 
+
+r"""import sys,os
+var1 = r'cd C:\Users\grusi\Gabriel\Programacao\MeusProjetos\Automate-the-Boring-Stuff-with-Python' #Tentei ativar o venv por dentro do programa, mas por hora não deu certo, uma outra ideia é tentar chamar o arquivo diretamente, ao invés de usar o cd dir + venv\scripts\activate
+print(var1) 
+os.system(var1)
+os.system('dir')
+var2 = r'venv\scripts\activate'
+print(var2)
+os.system(var2)"""
+
 import sys, pyperclip
 TEXT = {'bomdia':"""Bom dia, tudo bem?""",'boanoite':'''Vou ir dormir já, tenha uma boa noite, até depois.''','bem':'''Estou bem e você? Como estão as coisas por aí?'''}
 
